b759efc80fd74ba93ae22e0ae7b1b4d7acd8c2ea
keisuke <keisukent0228@i.softbank.jp>
2021-01-15 18:52:25 +0900

    profileのnewアクションの追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,26 @@
 class ProfilesController < ApplicationController
   def index
-    
+    @profiles = Profile.all 
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.create(profile_params)
+    @profile.save
   end
 
   def show
     
+  end
+
+  private
+  
+  def profile_params
+    params.require(:profile).permit(
+      :company, :name, :employee_num, :start_time, :ending_time, :regular_holiday, :tel, :postal_code, :address
+    )
   end
 end
