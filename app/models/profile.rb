2a2f43b4e39bfd8a0eeb78938d4546c712ae99c1
keisuke <keisukent0228@i.softbank.jp>
2021-01-17 12:41:35 +0900

    validateの追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,13 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :company, presence: true
+  validates :name, presence: true
+  validates :employee_num, presence: true
+  validates :start_time, presence: true
+  validates :ending_time, presence: true
+  validates :regular_holiday, presence: true
+  validates :tel, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
 end
